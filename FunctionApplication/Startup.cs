b4206a1ed3bb7fea3cc6cc8b70a2d9c9521b2801
Mika Berglund <mika.berglund@meteoriitti.com>
2019-07-31 10:51:40 +0300

    Removed unused usings.

--- FunctionApplication/Startup.cs
+++ FunctionApplication/Startup.cs
@@ -4,9 +4,6 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 [assembly: WebJobsStartup(typeof(Startup))]
 
